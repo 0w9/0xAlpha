2c910b830494a55ccb63d2cffecc726d32c2959b
0w9 <79471472+0w9@users.noreply.github.com>
2022-03-09 08:28:52 +0100

    fix(twitter scraper): the twitter scraper was not able to fetch tweets without a problem before
    The new fix allowes the twiter scraper to scrape tweets and format them into a array.

--- backend/scrapers/twitter.js
+++ backend/scrapers/twitter.js
@@ -6,18 +6,18 @@ const pino = require("pino");
 
 let twitterConfig = JSON.parse(fs.readFileSync('/Users/lennard/nft-portfolio-management-1/backend/scrapers/config.json'));
 
-const logger = pino({transport: {target: 'pino-pretty',options: {colorize: 'true'}},});
+//const logger = pino({transport: {target: 'pino-pretty',options: {colorize: 'true'}},});
 
 async function getTweetsById(user_id) {
 		let uri = `https://api.twitter.com/2/users/${user_id}/tweets?exclude=replies,retweets&tweet.fields=id,created_at,text,author_id`
 	    const res = await axios.get(uri, {headers: {'Authorization': twitterConfig.bearer_token}}).then(res => res.data);
-	//console.log(res.data);	
+
 		let tweetsMap = []
 		tweetsMap = res.data?.map(x => ({id: x.id, created_at: x.created_at, text: x.text.replace(/(\r\n|\n|\r)/gm, " "), author_id: x.author_id})); 
 	return tweetsMap;
 }
 
-function fetchTweets() {
+async function fetchTweets() {
     console.time("Fetch tweets");
 	
 	const ids = fs.readFileSync('/Users/lennard/nft-portfolio-management-1/backend/scrapers/id_collection.json');
@@ -26,11 +26,14 @@ function fetchTweets() {
 	const twitter_id_arr = Object.keys(id_json).map(key => id_json[key][0].twitter);	
 	const all_tweets = [];
 
-	for(let i=0; i < twitter_id_arr.length; i++) {
-		let res = getTweetsById(twitter_id_arr[i]);	
-		all_tweets.push(res);	
+	for(let i = 0; i < twitter_id_arr.length; i++) {
+		let uri = `https://api.twitter.com/2/users/${twitter_id_arr[i]}/tweets?exclude=replies,retweets&tweet.fields=id,created_at,text,author_id`
+	    const res = await axios.get(uri, {headers: {'Authorization': twitterConfig.bearer_token}}).then(res => res.data);
+
+		let tweetsMap = res.data?.map(x => ({id: x.id, created_at: x.created_at, text: x.text.replace(/(\r\n|\n|\r)/gm, " "), author_id: x.author_id})); 
+		all_tweets.push(tweetsMap);
 	}
-	console.log(all_tweets.forEach(tweet => console.log(tweet.id)));
+	console.log(all_tweets);
 
 	console.timeEnd("Fetch tweets");
 	
